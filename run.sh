6cd1f99b54cd90e865d5e7f8cd0b7ab5c4a5afa1
Kingsuk Biswas <kingsuk2k@gmail.com>
2023-08-24 00:09:24 +0530

    some parts done

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Constants
-functions=('Files Manipulation' 'News' 'Weather' 'Sports')
+functions=('Files Manipulation' 'Search Files or Text' 'News' 'Weather' 'Sports')
+search_options=('Files' 'Directory' 'Text')
 groups=("Owner" "Group" "Others")
-dir_operation=('Create Directory' 'Delete Directory' 'Update Directory Name' 'Check Permissions' 'Edit Permissions' 'File Operations' 'Organizer')
+dir_operation=('Create' 'Delete ' 'Update Name' 'Check Permissions' 'Edit Permissions' 'Directory Size' 'Current Path' 'File Operations' 'Organizer')
 files_operation=('Create File' 'Delete File' 'Write/Edit File' 'Update FileName' 'Edit Permissions' 'File Info' 'Read File')
 
 #Print the element of any array
@@ -16,7 +17,7 @@ print_array() {
 
 # Read any value from the terminal
 read_value() {
-    read -r -p"Enter the option, Press E to exit, M for Previous Menu: " value
+    read -r -e -p"Press E to exit, M for Previous Menu: " value
     echo "$value"
 }
 
@@ -179,106 +180,177 @@ list_directory() {
     echo -e "\n"
 }
 
+dir_traversal() {
+    op1=$1
+    case $op1 in
+    "..")
+        cd ..
+        ;;
+    "...")
+        cd ../..
+        ;;
+    *)
+        if [ -d "$op1" ]; then
+            cd "$op1"
+        fi
+        ;;
+    esac
+    dir=$(pwd)
+    echo -e "Current Directory: \n"
+    list_directory "$dir" "0"
+}
+
+operations() {
+    local n=$1
+    shift # Remove the first argument (index) from the argument list
+    for e in "${!dir_operation[@]}"; do
+        if ((e < n)); then
+            printf "%-*s  %s\n" "30" "$((e + 1)). ${dir_operation[$e]}" "$((e + 3 + n)). ${dir_operation[$e]}"
+        else
+            printf "%-*s\n" "30" "$((e + 1)). ${dir_operation[$e]}"
+        fi
+        # printf "%-*s  %s\n" "30" "$((e + 1)). ${dir_operation[$e]}" "$((j + 1)). ${dir_operation[$j]}"
+    done
+}
+
 #handles all directory operations
 directory_operations() {
+    option=""
     while true; do
-        dir=$(pwd)
-        echo -e "Current Directory: \n"
-        list_directory "$dir" "0"
-        op=1
-        read -r -e -p "Press '..' to go to previous directory, '...' to jump two directory back 'O' for options and write the name of directory to move to it: " val
-        if [[ $val == '..' ]]; then
-            cd ..
-        elif [[ $val == 'O' || $val == 'o' ]]; then
-            op=0
-        elif [[ $val == '...' ]]; then
-            cd ../..
-        elif [[ -d $val ]]; then
-            cd "$val"
-        else
-            echo "Invalid Input. Try Again ..."
-        fi
-        if [[ $op == 0 ]]; then
-            echo -e "\nThe following operations could be perfromed"
-            print_array "${dir_operation[@]}"
-            echo "Write the name of directory to enter the directory, and 'P' to exit the directory."
-            option=$(read_value)
-            case $option in
-            "1")
-                read -r -e -p "Enter the name of directory: " name
-                echo "$name"
-                mkdir "$name"
-                echo "Directory created ..."
-                ;;
-            "2")
-                read -r -e -p "Enter the directory name to be deleted: " -a input_array
-                name="${input_array[*]}"
-                if [ -n "$(find "$name" -maxdepth 1 \( -type f -o -type d \) -print -quit)" ]; then
-                    clear
-                    echo -e "The directory is not empty\n"
-                    list_directory "$name" "0"
-                    read -r -p "Do you want to delete it with all sub files and directory? (Y/N)" value
-                    if [[ $value == 'y' || $value == 'Y' ]]; then
-                        rm -r "$name"
-                        echo "Directory deleted.."
-                    else
-                        echo "Operation Cancelled"
-                    fi
-                else
-                    rm -r "$name"
-                    echo "Directory deleted"
-                fi
-
-                ;;
-            "3")
-                read -r -p "Enter the name of directory: " name
-                read -r -p "Enter the directory new name: " nname
-                mv "$name" "$nname"
-                echo "Directory renamed from $name to $nname"
-                ;;
-
-            "4")
-                read -r -p "Enter the name of directory: " name
-                perm=$(stat --format="%A" "$name")
-                clear
-                echo "$(file_or_directory_permission $perm $name)"
-
-                ;;
-
-            "5")
-                permission=$(edit_permissions)
-                echo "$permission"
-                chmod "$permission" "$name"
-                echo "Directory permission changed.."
-                ;;
-
-            "6")
-                clear
-                file_operations $dir
-                option='m'
-                ;;
-            "7")
-                dir_organizer
-                ;;
-
-            "e" | "E")
-                echo "See you soon"
-                exit
-                ;;
-            "m" | "M")
-                return
-                ;;
-            *)
-                echo "Invalid operation: $operation"
-                ;;
-
-            esac
-            if [[ $option =~ [1-7] ]]; then
-                echo "Press any key to continue...."
-                read -n 1 -s
-            fi
-        fi
         clear
+        dir_traversal "$option"
+        echo -e "The following operations could be perfromed"
+        printf "%-*s  %s\n" "30" "<- Directory" "<-Files"
+        n=${#dir_operation[@]}
+        operations $(($n - 2))
+        echo -e "\nWrite the name of directory to enter the directory, \nPress '..' to go to previous directory, \nPress '...' to jump two directories back"
+        option=$(read_value)
+        if [[ $option != '..' || $option != '...' ]];then
+        case $option in
+        "1")
+            read -r -e -p "Enter the name of directory: " name
+            echo "$name"
+            mkdir "$name"
+            echo "Directory created ..."
+            ;;
+        "2")
+            read -r -e -p "Enter the directory name to be deleted: " -a input_array
+            name="${input_array[*]}"
+            if [ -n "$(find "$name" -maxdepth 1 \( -type f -o -type d \) -print -quit)" ]; then
+                clear
+                echo -e "The directory is not empty\n"
+                list_directory "$name" "0"
+                read -r -p "Do you want to delete it with all sub files and directory? (Y/N)" value
+                if [[ $value == 'y' || $value == 'Y' ]]; then
+                    rm -r "$name"
+                    echo "Directory deleted.."
+                else
+                    echo "Operation Cancelled"
+                fi
+            else
+                rm -r "$name"
+                echo "Directory deleted"
+            fi
+
+            ;;
+        "3")
+            read -r -p "Enter the name of directory: " name
+            read -r -p "Enter the directory new name: " nname
+            mv "$name" "$nname"
+            echo "Directory renamed from $name to $nname"
+            ;;
+
+        "4")
+            read -r -p "Enter the name of directory: " name
+            if [[ $name == '.' ]]; then
+                name=$(pwd)
+            fi
+            perm=$(stat --format="%A" "$name")
+            echo "$(file_or_directory_permission $perm $name)"
+            echo "Press any key to continue..."
+            read -n 1 -s
+            ;;
+
+        "5")
+            permission=$(edit_permissions)
+            echo "$permission"
+            chmod "$permission" "$name"
+            echo "Directory permission changed.."
+            ;;
+        "6")
+            echo "Not implemented"
+            ;;
+        "7")
+            echo -e "\nCurrent Path : $(pwd)"
+            echo -e "\nPress any key to continue...."
+            read -n 1 -s
+            ;;
+
+        "8")
+            clear
+            file_operations $dir
+            option='m'
+            ;;
+        "9")
+            dir_organizer
+            ;;
+
+        "e" | "E")
+            echo "See you soon"
+            exit
+            ;;
+        "m" | "M")
+            return
+            ;;
+        "10")
+            if ! [ -f "$name" ]; then
+                touch "$name"
+                echo "File created: $name"
+            else
+                echo "File already present !!!"
+            fi
+            ;;
+        "11")
+            if [[ $name == *"*"* ]]; then
+                rm "$name"
+            elif [ -f "$name" ]; then
+                rm "$name"
+                echo "File deleted: $name"
+            else
+                echo "File does not exist: $name"
+            fi
+            ;;
+        "-")
+            echo "Edit file ${name}"
+            ;;
+
+        "12")
+            read -r -p "Enter the file new name: " nname
+            mv "$name" "$nname"
+            echo "File renamed from $name to $nname"
+            ;;
+
+        "14")
+            permission=$(edit_permissions)
+            echo "$permission"
+            chmod "$permission" "$name"
+            echo "File permission changed.."
+            ;;
+
+        "13")
+            file_info=($(ls -l $name))
+            echo -e "The file info:\nName: $(file_or_directory_permission "${file_info[0]}" "$name")"
+            echo -e "Owner: ${file_info[2]}\nSize: ${file_info[4]} bytes\nLast Modified Date: ${file_info[5]} ${file_info[6]} - Time: ${file_info[7]}\n"
+
+            ;;
+        *)
+            continue
+            ;;
+
+        esac
+        echo -e "\nPress any key to continue...."
+        read -n 1 -s
+        fi
     done
 
 }
@@ -308,7 +380,7 @@ file_operations() {
                 ;;
             "2")
                 if [[ $name == *"*"* ]]; then
-                    rm $name
+                    rm "$name"
                 elif [ -f "$name" ]; then
                     rm "$name"
                     echo "File deleted: $name"
@@ -354,6 +426,66 @@ file_operations() {
     done
 }
 
+#!/bin/bash
+
+clear_part_of_screen() {
+    local row=$1
+    local col=$2
+    tput cup $row $col # Move cursor to the specified position
+    tput ed            # Clear from the cursor position to the end of the screen
+}
+
+# Example usage  # Clears from row 5, column 10 to the end of the screen
+
+#search files
+
+search_operations() {
+    root=$(pwd)
+    echo -e "The following search operations can be performed:"
+    print_array "${search_options[@]}"
+    while true; do
+
+        option=$(read_value)
+        case $option in
+        "1")
+            dir_traversal
+            # echo -e "Press . if want to search in present directory.\n"
+            read -p"Press '.' to search in working directory, S to select directory " dir
+            if [[ $dir == 'S' ]]; then
+                option=""
+                while true; do
+                    clear_part_of_screen 6 0
+                    dir_traversal $option
+                    echo -e "\nWrite the name of directory to enter the directory, \nPress '..' to go to previous directory \nPress '...' to jump two directories back\nPress 'S' to Stop"
+                    option=$(read_value)
+                    if [[ $option == 'S' ]]; then
+                        root=$(pwd)
+                        break
+                    fi
+                done
+            elif [[ $dir != '.' ]]; then
+                echo "Invalid Input!!!"
+            fi
+
+            read -r -e -p"Enter the file name to be search: " name
+            path=$(find "$root" -name .git -prune -o -type f -name "$name" -print)
+            echo -e "\n$path"
+            echo "Press any key to continue..."
+            read -n 1 -s
+
+            ;;
+        "e" | "E")
+            echo "See you soon"
+            exit
+            ;;
+        "m" | "M")
+            return
+            ;;
+        esac
+        clear_part_of_screen 4 0
+    done
+}
+
 #start of the program
 main() {
     while true; do
@@ -367,6 +499,10 @@ main() {
             clear
             echo -e "Select the type of manipulation you want to do: \n"
             directory_operations
+            ;;
+        "2")
+            clear
+            search_operations
             ;;
 
         "e" | "E")
